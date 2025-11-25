3cc29e2ae9d50f23eb9ae43916ec14014e2ab852
raziel <tvindzon@gmail.com>
2025-11-25 15:48:40 +0700

    create app\bot\config\models\video.py

---
+++ app/bot/config/models/video.py
@@ -0,0 +1,52 @@
+from typing import Optional, Dict
+
+from pydantic import BaseModel
+
+
+class Kinopoisk(BaseModel):
+    """Модель рекомендательной системы для кинопоиска."""
+
+    SERVICE_NAME: str = "Kinopoisk"
+    SERVICE_ID: str = "kinopoisk"
+
+    API_KEY: Optional[str] = None
+
+    # URL для запросов
+    URL_SEARCH_VIDEO_NAME: str = (
+        "https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit={}&query={}"
+    )
+    URL_SEARCH_UNIVERSAL_VIDEO: str = (
+        "https://api.kinopoisk.dev/v1.4/movie?page=1&limit={}"
+    )
+
+    HEADERS: Dict = {
+        "accept": "application/json",
+        "X-API-KEY": None,
+    }
+
+
+class ViewingAdvieModels(BaseModel):
+    """Общий класс для моделей для рекомендаций по названию фильма."""
+    
+    SERVICE_NAME: str = "ViewingAdvie"
+    SERVICE_ID: str = "viewing_advice"
+
+    # Данные кнопок для подключаемых моделей
+    CALLBACK_BUTTON_TEXT_KINOPOISK: str = "1⃣ Kinopoisk"
+    CALLBACK_BUTTON_DATA_KINOPOISK: str = "viewing_advice kinopoisk"
+
+    kinopoisk: Kinopoisk = Kinopoisk()
+
+
+class VideoModels(BaseModel):
+    """Общий класс для генерации видео моделей."""
+
+    SERVICE_NAME: str = "Video"
+    SERVICE_ID: str = "video"
+
+    # Данные кнопок для подключаемых моделей
+    CALLBACK_BUTTON_TEXT_VIEWING_ADVICE: str = "📚 Совет По Названию Фильма"
+    CALLBACK_BUTTON_DATA_VIEWING_ADVICE: str = "video viewing_advice"
+    START_BOT_MENU_REPLY_TEXT: str = "🎦 Видео"
+
+    viewing_advice: ViewingAdvieModels = ViewingAdvieModels()
